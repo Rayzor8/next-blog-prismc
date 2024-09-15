fb842fb6682acfb0fb38beb9b87b842875dc976f
Ray <rayhidayat220@gmail.com>
2024-09-16 05:25:19 +0700

    feat: setup and cr8 hero section

--- src/slices/Hero/index.tsx
+++ src/slices/Hero/index.tsx
@@ -1,5 +1,10 @@
-import { Content } from "@prismicio/client";
+"use client";
+
+import { useRef, useEffect } from "react";
+import { Content, KeyTextField } from "@prismicio/client";
 import { SliceComponentProps } from "@prismicio/react";
+import gsap from "gsap";
+import Bounded from "@/ui/Bounded";
 
 /**
  * Props for `Hero`.
@@ -10,15 +15,86 @@ export type HeroProps = SliceComponentProps<Content.HeroSlice>;
  * Component for "Hero" Slices.
  */
 const Hero = ({ slice }: HeroProps): JSX.Element => {
+  const component = useRef(null);
+
+  useEffect(() => {
+    const ctx = gsap.context(() => {
+      const timeline = gsap.timeline();
+
+      timeline.fromTo(
+        ".name-animation",
+        { x: -100, opacity: 0, rotate: -10 },
+        {
+          x: 0,
+          opacity: 1,
+          rotate: 0,
+          ease: "bounce.out",
+          duration: 0.8,
+          stagger: {
+            each: 0.1,
+            from: "random",
+          },
+        }
+      );
+
+      timeline.fromTo(
+        ".job-animation",
+        {
+          x: 50,
+          opacity: 0,
+          rotate: 10,
+        },
+        {
+          x: 0,
+          opacity: 1,
+          rotate: 0,
+          ease: "bounce.out",
+          duration: 0.8,
+        }
+      );
+    }, component);
+    return () => ctx.revert();
+  }, []);
+
+  const renderLetters = (name: KeyTextField, key: string) => {
+    if (!name) return;
+    return name.split("").map((letter, index) => (
+      <span
+        key={index}
+        className={`name-animation name-animation-${key}-index inline-block opacity-0 `}
+      >
+        {letter}
+      </span>
+    ));
+  };
+
   return (
-    <section
+    <Bounded
       data-slice-type={slice.slice_type}
       data-slice-variation={slice.variation}
+      ref={component}
     >
-      <span className="text-red-400">{slice.primary.first_name}</span>
-      <p>{slice.primary.last_name}</p>
-      <p>{slice.primary.tag_line}</p>
-    </section>
+      <div className="grid min-h-[70vh] grid-cols-1 md:grid-cols-2 items-center">
+        <div className="col-start-1 md:row-start-1">
+          <h1
+            className="mb-6 text-[clamp(3rem,16vmin,7rem)] font-extrabold leading-none tracking-wide"
+            aria-label={`${slice.primary.first_name}${slice.primary.last_name}`}
+          >
+            <span className="block text-slate-200">
+              {renderLetters(slice.primary.first_name, "first")}
+            </span>
+
+            <span className="block text-red-400">
+              {renderLetters(slice.primary.last_name, "last")}
+            </span>
+          </h1>
+          <span className="job-animation block bg-gradient-to-tr from-blue-400 via-red-400 to-red-800 bg-clip-text font-bold tracking-widest text-transparent opacity-0 text-3xl lg:text-4xl">
+            {slice.primary.tag_line}
+          </span>
+        </div>
+        <div className="bg-white w-full h-full"></div>
+      </div>
+    </Bounded>
   );
 };
 
